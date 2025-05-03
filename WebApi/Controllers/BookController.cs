f25a1ad7d0d0d50117ea5430689933b7de1a2df3
Özberk Şen <96706849+Ozberksenn@users.noreply.github.com>
2025-05-04 00:32:27 +0300

    first commit

---
+++ WebApi/Controllers/BookController.cs
@@ -0,0 +1,50 @@
+using Microsoft.AspNetCore.Mvc;
+using WebApi;
+
+namespace WepApi.AddController
+{
+    [ApiController]
+    [Route("[controller]s")]
+    public class BookController : ControllerBase
+    {
+        private static List<Book> BookList = new List<Book>(){
+
+            new Book {
+                Id = 1,
+                Title = "Learn Startup",
+                GenreId = 1,
+                PageCount = 200,
+                publishDate = new DateTime(2001,06,12),
+            },
+            new Book {
+                Id = 2,
+                Title = "Herland",
+                GenreId = 2,
+                PageCount = 220,
+                publishDate = new DateTime(2010,05,23),
+            },
+             new Book {
+                Id = 3,
+                Title = "Dune",
+                GenreId = 2,
+                PageCount = 540,
+                publishDate = new DateTime(2002,05,23),
+            }
+        };
+
+        [HttpGet]
+        public List<Book> GetBooks()
+        {
+            var bookList = BookList.OrderBy(x => x.Id).ToList();
+            return bookList;
+        }
+        [HttpGet("{id}")]
+        public Book GetById(int id)
+        {
+            var book = BookList.Where(x => x.Id == id).SingleOrDefault();
+            return book ?? new Book();
+
+        }
+
+    }
+}
