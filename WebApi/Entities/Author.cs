d77ef59d7b6abdc97e0430c9cc2698cde55ffabf
Özberk Şen <96706849+Ozberksenn@users.noreply.github.com>
2025-05-22 23:35:40 +0300

    fixed

--- WebApi/Entities/Author.cs
+++ WebApi/Entities/Author.cs
@@ -6,8 +6,8 @@ namespace WebApi.Entities
     {
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
-        public string Name { get; set; }
-        public int LastName { get; set; }
-        public int BirthDate { get; set; }
+        public string? Name { get; set; }
+        public string? LastName { get; set; }
+        public DateTime BirthDate { get; set; }
     }
 }
