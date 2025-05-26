b901d9cdf09e48d8f5432dc6c68c4736fbb81aed
Özberk Şen <96706849+Ozberksenn@users.noreply.github.com>
2025-05-26 18:50:02 +0300

    updated

--- WebApi/Common/MappingProfile.cs
+++ WebApi/Common/MappingProfile.cs
@@ -1,6 +1,7 @@
 
 using AutoMapper;
 using WebApi.Application.AuthorOperations.Commands.CreateAuthor;
+using WebApi.Application.AuthorOperations.Queries.GetAuthorDetail;
 using WebApi.Application.AuthorOperations.Queries.GetAuthors;
 using WebApi.Application.GenreOperations.Queries;
 using WebApi.Application.GenreOperations.Queries.GetGenreDetail;
@@ -23,6 +24,7 @@ namespace WebApi.Common
             CreateMap<Genre, GenresViewModel>(); // dökümanda açıkla. Genre yi GenreViewModel e dönüştürdük.
             CreateMap<Genre, GenreDetailViewModel>();
             CreateMap<Author, AuthorsViewModel>();
+            CreateMap<Author, AuthorDetailViewModel>();
             CreateMap<CreateAuthorViewModel, Author>();
 
         }
