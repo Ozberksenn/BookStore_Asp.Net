5b2822725521c48ebd1a0054480b48c428eb7167
Özberk Şen <96706849+Ozberksenn@users.noreply.github.com>
2025-05-28 01:32:14 +0300

    test

--- WebApi/Entities/Book.cs
+++ WebApi/Entities/Book.cs
@@ -7,7 +7,7 @@ namespace WebApi.Entities
 
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
-        public string Title { get; set; }
+        public string? Title { get; set; }
         public int GenreId { get; set; }
         public Genre Genre { get; set; }
         public int AuthorId { get; set; }
